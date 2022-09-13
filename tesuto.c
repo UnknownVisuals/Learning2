29c3cd3bb9cf10a438f9e62c656ca5e881d58733
Reynaldhi <reynaldig40@gmail.com>
2022-09-13 13:39:13 +0700

    Adding .c file

--- tesuto.c
+++ tesuto.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main {
-    printf("Hello World!");
+int main () {
+    int n, i, j;
+
+    system("cls");
+
+    printf("+-------------------+\n");
+    printf("|   Hello World!    |\n");
+    printf("+-------------------+\n\n");
+
+    printf("Hello how many times? ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
